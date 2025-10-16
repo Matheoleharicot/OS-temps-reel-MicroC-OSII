c4ff23e7c6fbfac73feda85847756e7c5f741c4d
Mathéo Morin <91942291+Matheoleharicot@users.noreply.github.com>
2025-10-16 14:12:48 +0200

    first commit

---
+++ TP/obj/default/hello_ucosii.d
@@ -0,0 +1,38 @@
+obj/default/hello_ucosii.o: hello_ucosii.c \
+ h:\real_time\tp_bsp\hal\inc\includes.h \
+ h:\real_time\tp_bsp\hal\inc\os_cpu.h \
+ h:\real_time\tp_bsp\hal\inc\sys/alt_irq.h \
+ h:\real_time\tp_bsp\hal\inc\nios2.h \
+ h:\real_time\tp_bsp\hal\inc\alt_types.h H:/Real_Time/TP_bsp/system.h \
+ H:/Real_Time/TP_bsp/linker.h h:\real_time\tp_bsp\ucosii\inc\os_cfg.h \
+ h:\real_time\tp_bsp\hal\inc\sys\alt_alarm.h \
+ h:\real_time\tp_bsp\hal\inc\sys\alt_llist.h \
+ h:\real_time\tp_bsp\hal\inc\priv\alt_alarm.h \
+ h:\real_time\tp_bsp\ucosii\inc\ucos_ii.h \
+ h:\real_time\tp_bsp\hal\inc\os_cpu.h
+
+h:\real_time\tp_bsp\hal\inc\includes.h:
+
+h:\real_time\tp_bsp\hal\inc\os_cpu.h:
+
+h:\real_time\tp_bsp\hal\inc\sys/alt_irq.h:
+
+h:\real_time\tp_bsp\hal\inc\nios2.h:
+
+h:\real_time\tp_bsp\hal\inc\alt_types.h:
+
+H:/Real_Time/TP_bsp/system.h:
+
+H:/Real_Time/TP_bsp/linker.h:
+
+h:\real_time\tp_bsp\ucosii\inc\os_cfg.h:
+
+h:\real_time\tp_bsp\hal\inc\sys\alt_alarm.h:
+
+h:\real_time\tp_bsp\hal\inc\sys\alt_llist.h:
+
+h:\real_time\tp_bsp\hal\inc\priv\alt_alarm.h:
+
+h:\real_time\tp_bsp\ucosii\inc\ucos_ii.h:
+
+h:\real_time\tp_bsp\hal\inc\os_cpu.h:
